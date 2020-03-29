2a571d069eb10a8351d708911ec9358d936101b1
DCubix <diego95lopes@live.com>
2020-03-29 00:51:34 -0400

    Progress!

--- framework.js
+++ framework.js
@@ -9,11 +9,32 @@ def toScreen(x, y, z):
  */
 
 Math.isometricToScreen = function(x, y, z) {
-	return [ x - y, (x + y) / 2.0 - z ];
+	z = z || 0;
+	return [ ~~(x - y), ~~((x + y) / 2.0 - z) ];
 };
 
 Math.isometricFromScreen = function(x, y) {
-	return [ (2.0 * y + x) / 2.0, (2.0 * y - x) / 2.0 ];
+	return [ ~~((2.0 * y + x) / 2.0), ~~((2.0 * y - x) / 2.0) ];
+};
+
+Math.distance = function(x1, y1, x2, y2) {
+	let dx = x2 - x1,
+		dy = y2 - y1;
+	return Math.sqrt(dx * dx + dy * dy);
+};
+
+Math.clamp = function(a, mi, ma) {
+	return Math.max(Math.min(a, ma), mi);
+};
+
+Math.smoothstep = function(e0, e1, x) {
+	let t = Math.clamp((x - e0) / (e1 - e0), 0.0, 1.0);
+	return t * t * (3.0 - 2.0 * t);
+};
+
+Math.remap = function(val, fmi, fma, tmi, tma) {
+	let ratio = (val - fmi) / (fma - fmi);
+	return tmi + ratio * (tma - tmi);
 };
 
 class Renderer {
@@ -37,6 +58,8 @@ class Renderer {
 
 		this.context.imageSmoothingEnabled = false;
 		this.backContext.imageSmoothingEnabled = false;
+
+		this.imageData = this.backContext.getImageData(0, 0, this.backCanvas.width, this.backCanvas.height);
 	}
 
 	get camera() {
@@ -53,6 +76,58 @@ class Renderer {
 		this.cameraY = cam[1];
 	}
 
+	pixel(x, y, color) {
+		if (x < 0 || x >= this.imageData.width || y < 0 || y >= this.imageData.height)
+			return;
+
+		let pixels = this.imageData.data;
+		color = color || [1.0, 1.0, 1.0];
+
+		let i = (x + y * this.imageData.width) * 4;
+
+		let r = ~~(color[0] * 255) % 256,
+			g = ~~(color[1] * 255) % 256,
+			b = ~~(color[2] * 255) % 256;
+		if (pixels[i + 0] != r) pixels[i + 0] = r;
+		if (pixels[i + 1] != g) pixels[i + 1] = g;
+		if (pixels[i + 2] != b) pixels[i + 2] = b;
+		pixels[i + 3] = 255;
+	}
+
+	/**
+	 * 
+	 * @param {ImageData} image 
+	 * @param {*} x 
+	 * @param {*} y 
+	 * @param {*} w 
+	 * @param {*} h 
+	 * @param {*} sx 
+	 * @param {*} sy 
+	 * @param {*} sw 
+	 * @param {*} sh 
+	 */
+	draw(image, dx, dy, dw, dh, sx, sy, sw, sh, color) {
+		color = color || [1.0, 1.0, 1.0];
+		let pixels = image.data;
+		for (let k = 0; k < dw * dh; k++) {
+			let x = k % dw;
+			let y = ~~Math.floor(k / dw);
+			let cx = Math.remap(x, 0, dw, 0, sw),
+				cy = Math.remap(y, 0, dh, 0, sh);
+			let ix = cx + sx,
+				iy = cy + sy;
+			let si = (ix + iy * image.width) * 4;
+			if (ix < 0 || ix >= image.width || iy < 0 || iy >= image.height) continue;
+			if (pixels[si + 3] < 128) continue;
+
+			let col = [0, 0, 0];
+			col[0] = (pixels[si + 0]/255) * color[0];
+			col[1] = (pixels[si + 1]/255) * color[1];
+			col[2] = (pixels[si + 2]/255) * color[2];
+			this.pixel(x + dx, y + dy, col);
+		}
+	}
+
 	raw(image, x, y, ox, oy, sx, sy, sw, sh, color) {
 		let cam = this.camera;
 
@@ -62,17 +137,20 @@ class Renderer {
 		sy = sy || 0;
 		sw = sw || image.width;
 		sh = sh || image.height;
-		color = color || [255, 255, 255, 1.0];
+		color = color || [1.0, 1.0, 1.0];
 
 		let ix = x - (~~(sw * ox));
 		let iy = y - (~~(sh * oy));
 
 		this.commands.push({
+			type: "image",
 			image: image,
 			x: ix - cam[0],
 			y: iy - cam[1],
 			source: [sx, sy, sw, sh],
-			color: color
+			color: color,
+			ex: x,
+			ey: y
 		});
 	}
 
@@ -88,10 +166,13 @@ class Renderer {
 		r = r || 0;
 		g = g || 0;
 		b = b || 0;
-		this.backContext.save();
-		this.backContext.fillStyle = `rgb(${r}, ${g}, ${b})`;
-		this.backContext.fillRect(0, 0, this.backCanvas.width, this.backCanvas.height);
-		this.backContext.restore();
+		// for (let y = 0; y < this.imageData.height; y++) {
+		// 	for (let x = 0; x < this.imageData.width; x++) {
+		// 		this.pixel(x, y, [r, g, b]);
+		// 	}
+		// }
+		this.backContext.fillStyle = `rgb(${r * 255}, ${g * 255}, ${b * 255})`;
+		this.backContext.fillRect(0, 0, this.imageData.width, this.imageData.height);
 	}
 
 	/**
@@ -108,12 +189,10 @@ class Renderer {
 		if (sorting === "back") {
 			this.commands = this.commands.reverse();
 		} else if (sorting === "y") {
-			this.commands.sort(function(a, b) { return a.y < b.y ? -1 : 1; });
+			this.commands.sort(function(a, b) { return a.ey > b.ey ? -1 : 1; });
 		} else if (sorting === "y-") {
-			this.commands.sort(function(a, b) { return a.y > b.y ? -1 : 1; });
+			this.commands.sort(function(a, b) { return a.ey < b.ey ? -1 : 1; });
 		}
-
-		let cam = this.camera;
 
 		// Cull things out
 		let culledCommands = [];
@@ -133,13 +212,16 @@ class Renderer {
 
 		context.save();
 		for (let cmd of culledCommands) {
-			context.globalAlpha = cmd.color[3];
+			context.globalAlpha = cmd.color[0];
+			
 			let x = cmd.x,
 				y = cmd.y;
+			//this.draw(cmd.image, ~~x, ~~y, cmd.source[2], cmd.source[3], cmd.source[0], cmd.source[1], cmd.source[2], cmd.source[3], cmd.color);
 			context.drawImage(cmd.image, cmd.source[0], cmd.source[1], cmd.source[2], cmd.source[3], ~~x, ~~y, cmd.source[2], cmd.source[3]);
 		}
 		context.restore();
 
+		//this.backContext.putImageData(this.imageData, 0, 0);
 		this.context.drawImage(this.backCanvas, 0, 0, this.backCanvas.width * this.pixelSize, this.backCanvas.height * this.pixelSize);
 
 		this.commands = [];
@@ -169,6 +251,14 @@ class ContentHandler {
 			if (file.type === "image") {
 				let img = new Image();
 				img.onload = function() {
+					let cnv = document.createElement("canvas");
+					cnv.width = img.width;
+					cnv.height = img.height;
+					let ctx = cnv.getContext("2d");
+					ctx.drawImage(img, 0, 0);
+
+					let dat = ctx.getImageData(0, 0, cnv.width, cnv.height);
+
 					that.assets[file.name] = img;
 					ok++;
 					check();
@@ -189,6 +279,7 @@ class Entity {
 		this.y = 0;
 		this.tag = "";
 		this.life = -1;
+		this.visible = true;
 		this.dead = false;
 		this.__created = false;
 	}
@@ -257,6 +348,7 @@ class EntityHandler {
 		for (let ent of this.entities) {
 			if (tag && ent.tag !== tag) continue;
 			if (!ent.__created) continue;
+			if (!ent.visible) continue;
 			ent.onDraw(framework);
 		}
 	}
@@ -348,13 +440,23 @@ class Fw {
 			that.mouse[1] = ~~((e.clientY - rect.top) / pixelSize) + cam[1];
 		};
 		this.canvas.onmousedown = function(e) {
+			e.preventDefault();
+
 			that.mouseDown[e.button] = true;
 			that.mouseHold[e.button] = true;
+
+			let rect = that.canvas.getBoundingClientRect();
+			let cam = that.renderer.camera;
+			that.mouse[0] = ~~((e.clientX - rect.left) / pixelSize) + cam[0];
+			that.mouse[1] = ~~((e.clientY - rect.top) / pixelSize) + cam[1];
 		};
 		this.canvas.onmouseup = function(e) {
+			e.preventDefault();
+
 			that.mouseUp[e.button] = true;
 			that.mouseHold[e.button] = false;
 		};
+		this.canvas.oncontextmenu = function() { return false; };
 		this.canvas.focus();
 	}
 
--- game.js
+++ game.js
@@ -12,6 +12,59 @@ const PST_RUN_TOWARDS_THROW = 2;
 const PST_THROW = 3;
 const PST_DEAD = 4;
 
+
+class Ball extends Entity {
+	constructor() {
+		super();
+		this.z = 0;
+		this.ball = null;
+		this.shadow = null;
+		this.force = [0, 0, 0];
+		this.tag = "ball";
+	}
+
+	/**
+	 * 
+	 * @param {Fw} fw 
+	 */
+	onCreate(fw) {
+		this.ball = fw.content.assets["ball0"];
+		this.shadow = fw.content.assets["ball_shadow"];
+	}
+
+	/**
+	 * 
+	 * @param {Fw} fw 
+	 */
+	onDraw(fw) {
+		let spos = Math.isometricToScreen(this.x, this.y, 0);
+		fw.renderer.tile(this.shadow, spos[0], spos[1], 0.5, 0.5, 0, 1, 1);
+
+		let pos = Math.isometricToScreen(this.x, this.y, this.z);
+		fw.renderer.tile(this.ball, pos[0], pos[1], 0.5, 0.5, 0, 1, 1);
+	}
+
+	/**
+	 * 
+	 * @param {Fw} fw 
+	 */
+	onUpdate(fw, dt) {
+		this.force[2] -= 120 * dt;
+		
+		this.x += this.force[0] * dt;
+		this.y += this.force[1] * dt;
+		this.z += this.force[2] * dt;
+
+		this.force[0] *= 0.97;
+		this.force[1] *= 0.97;
+
+		if (this.z <= 0.0) {
+			this.force[2] = 0.0;
+			this.z = 0.0;
+		}
+	}
+}
+
 class BasePlayer extends Entity {
 	constructor() {
 		super();
@@ -23,7 +76,10 @@ class BasePlayer extends Entity {
 		this.direction = 0;
 
 		this.target = [0, 0];
+		this.vector = [0, 0];
 		this.state = PST_IDLE;
+
+		this.threw = false;
 	}
 
 	/**
@@ -41,43 +97,73 @@ class BasePlayer extends Entity {
 	 */
 	onUpdate(fw, dt) {
 		this.timer.tick(dt);
+		
+		let pos = Math.isometricFromScreen(fw.mouse[0], fw.mouse[1]);
+		pos[0] = ~~Math.round(pos[0] / 32.0) * 32;
+		pos[1] = ~~Math.round(pos[1] / 32.0) * 32;
 
-		let dx = this.target[0] - this.x;
-		let dy = this.target[1] - this.y;
-		let mag = Math.sqrt(dx * dx + dy * dy);
-		dx /= mag;
-		dy /= mag;
-
-		let angle = Math.atan2(dy, dx) + Math.PI * 2.0;
-		let dir = ~~Math.floor((angle - Math.PI / 8.0) / (Math.PI / 4.0)) % 8;
+		let dx = this.vector[0];
+		let dy = this.vector[1];
 
 		if (fw.isMousePressed(0)) {
-			let pos = Math.isometricFromScreen(fw.mouse[0], fw.mouse[1]);
 			this.goto(pos[0], pos[1]);
+		} else if (fw.isMousePressed(2)) {
+			let dx = pos[0] - this.x,
+				dy = pos[1] - this.y;
+			
+			let angle = Math.atan2(dy, dx) + Math.PI * 2.0;
+			this.direction = ~~Math.floor((angle - Math.PI / 8.0) / (Math.PI / 4.0)) % 8;
+
+			this.state = PST_THROW;
+			this.timer.frame = 0;
+			this.threw = true;
 		}
 
 		switch (this.state) {
 			default: break;
+			case PST_THROW: {
+				if (this.timer.frame % 8 === 5 && this.threw) {
+					let b = new Ball();
+					b.x = this.x;
+					b.y = this.y;
+					b.z = 32;
+
+					let dx = pos[0] - this.x,
+						dy = pos[1] - this.y,
+						dz = 1.0;
+					let mag = Math.sqrt(dx * dx + dy * dy + dz * dz);
+					b.force[0] = (dx / mag) * 300.0;
+					b.force[1] = (dy / mag) * 300.0;
+					b.force[2] = (dz / mag) * 500.0;
+					fw.entities.add(b);
+					this.threw = false;
+				} else if (this.timer.frame % 8 >= 7) this.state = PST_IDLE;
+			} break;
 			case PST_RUN_TOWARDS_THROW: {
 				if (this.timer.frame % 8 >= 7) this.state = PST_RUN_TOWARDS;
+
+				this.x += this.vector[0] * 150.0 * dt;
+				this.y += this.vector[1] * 150.0 * dt;
+
+				let angle = Math.atan2(dy, dx) + Math.PI * 2.0;
+				let dir = ~~Math.floor((angle - Math.PI / 8.0) / (Math.PI / 4.0)) % 8;
 				this.direction = dir;
 
-				this.x += dx * 16.0 * dt;
-				this.y += dy * 16.0 * dt;
-
-				if (mag <= 1.0) {
+				let mag = Math.distance(this.target[0], this.target[1], this.x, this.y);
+				if (mag <= 2.0) {
 					this.state = PST_IDLE;
 				}
 			} break;
 			case PST_RUN_TOWARDS: {
+				this.x += this.vector[0] * 150.0 * dt;
+				this.y += this.vector[1] * 150.0 * dt;
+
+				let angle = Math.atan2(dy, dx) + Math.PI * 2.0;
+				let dir = ~~Math.floor((angle - Math.PI / 8.0) / (Math.PI / 4.0)) % 8;
 				this.direction = dir;
 
-				this.x += dx * dt;
-				this.y += dy * dt;
-
-				console.log(dx);
-
-				if (mag <= 1.0) {
+				let mag = Math.distance(this.target[0], this.target[1], this.x, this.y);
+				if (mag <= 2.0) {
 					this.state = PST_IDLE;
 				}
 			} break;
@@ -97,30 +183,45 @@ class BasePlayer extends Entity {
 			default: break;
 			case PST_RUN_TOWARDS_THROW: frame = PAO_RUN_THROW + (8 * this.direction) + fr; break;
 			case PST_RUN_TOWARDS: frame = PAO_RUN + (8 * this.direction) + fr; break;
+			case PST_THROW: frame = PAO_STAND_THROW + (8 * this.direction) + fr; break;
 		}
 
 		let pos = Math.isometricToScreen(this.x, this.y, 0);
-		ren.tile(this.sprite, pos[0], pos[1], 0.5, 0.86, frame, 33, 8);
+		ren.tile(this.sprite, pos[0], pos[1]-30, 0.5, 0.5, frame, 33, 8);
 	}
 
 	goto(x, y) {
 		this.target = [x, y];
+		
+		let dx = x - this.x;
+		let dy = y - this.y;
+		let mag = Math.sqrt(dx * dx + dy * dy);
+		this.vector = [dx/mag, dy/mag];
+
 		if (this.state === PST_THROW) {
 			this.state = PST_RUN_TOWARDS_THROW;
 		} else {
 			this.state = PST_RUN_TOWARDS;
 		}
+		this.timer.frame = 0;
 	}
 }
 
 class BTBGame extends Game {
+	constructor() {
+		super();
+		this.player = new BasePlayer();
+	}
+
 	onLoad(content) {
 		content.addImage("char", "character.png");
 		content.addImage("blocks", "blocks.png");
+		content.addImage("ball0", "ball0.png");
+		content.addImage("ball_shadow", "ball_shadow.png");
 	}
 
 	onStart(fw) {
-		fw.entities.add(new BasePlayer());
+		fw.entities.add(this.player);
 	}
 
 	onUpdate(fw, dt) {
@@ -133,6 +234,8 @@ class BTBGame extends Game {
 		let content = fw.content.assets;
 		let ren = fw.renderer;
 
+		ren.camera = Math.isometricToScreen(this.player.x, this.player.y, 0);
+
 		ren.clear();
 
 		let mp = Math.isometricFromScreen(fw.mouse[0], fw.mouse[1]);
@@ -140,10 +243,28 @@ class BTBGame extends Game {
 		mp[1] = ~~Math.round(mp[1] / 32.0) * 32.0;
 		let mps = Math.isometricToScreen(mp[0], mp[1], 0.0);
 
+		for (let y = 0; y < 32; y++) {
+			for (let x = 0; x < 32; x++) {
+				let pos = Math.isometricToScreen(x * 32, y * 32, 0);
+
+				let mag = Math.distance(x * 32, y * 32, this.player.x, this.player.y);
+				let fade = 1.0 - Math.max(Math.min(mag / (5 * 32), 1.0), 0.0);
+				fade = Math.smoothstep(0.0, 0.8, fade);
+
+				let col = [fade, fade, fade];
+
+				ren.tile(content["blocks"], pos[0], pos[1]-32, 0.5, 0.5, 3, 16, 8, col);
+				ren.tile(content["blocks"], pos[0], pos[1], 0.5, 0.5, 11, 16, 8, col);
+			}
+		}
+
 		ren.tile(content["blocks"], mps[0], mps[1], 0.5, 0.5, 10, 16, 8);
+		ren.flush();
+
 		fw.entities.render(fw, "player");
+		fw.entities.render(fw, "ball");
 		
-		ren.flush("none", false);
+		ren.flush("y-");
 	}
 }
 
